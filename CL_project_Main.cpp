64da32c2ad607460b6a3dd58cd7cef01f7d71a33
ElaYJ <44601057+SYunJae@users.noreply.github.com>
2021-01-15 14:39:26 +0900

    Add files via upload

---
+++ CL_project_Main.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+
+
+int main(int argc, char* argv[], char *envp[])
+{
+	if (argc > 0)
+	{
+		int cnt = 0;
+		while (argv[cnt] != NULL)
+		{
+			printf(" --- %s\n", argv[cnt]);
+			cnt++;
+		}
+
+		cnt = 0;
+		while (envp[cnt] != NULL)
+		{
+			printf(" ~~~ %s\n", envp[cnt]);
+			cnt++;
+		}
+	}
+
+	return 0;
+}
