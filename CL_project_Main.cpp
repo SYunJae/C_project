9d5b8d6f65338d73b2a71bd99b5374a17209560d
ElaYJ <44601057+SYunJae@users.noreply.github.com>
2021-01-26 19:55:23 +0900

    Add files via upload

--- CL_project_Main.cpp
+++ CL_project_Main.cpp
@@ -2,6 +2,8 @@
 #include "CL_project_Header.h"
 
 
+#if 1
+
 int main(int argc, char* argv[], char* envp[])
 {
 	int count = 1;
@@ -45,3 +47,64 @@ int main(int argc, char* argv[], char* envp[])
 
 	return 0;
 }
+
+
+#else
+
+
+//	 #include <stdio.h>
+	 #include <cstdlib>
+
+	 
+///< | Data Area | >
+
+	 int variable = 0;
+	 
+	 int main(int argc, char * argv[], char * envp[])
+	 {
+	 	 static double num1 = 2.1, num2 = 3.0;
+	 	 
+	 	 const char* str = "Hello, World";
+
+	 	 
+//*	 	 | Stack Area |
+
+		 int num3 = 7, num4 = 3;
+
+	 	 double add = num1 + num2;
+	 	 double sub = num2 - num1;
+	 	 
+	 	 printf("%f + %f = %f \n\n", num1, num2, add);
+
+	 	 
+//#	 	 | Heap Area |
+	 
+	 	 int* arr = (int*)std::malloc(3 * sizeof(int));
+	 	 arr[0] = num3 * num4;
+	 	 arr[1] = num3 / num4; // Quotient
+	 	 arr[2] = num3 % num4; // Rest
+	 	 
+	 	 printf("%d %% %d = %d \n\n", num3, num4, arr[2]);
+	 	 
+	 	 free(arr);
+
+	 	 return 0;
+	 }
+
+/*
+		 printf(" Global Variable : 0x%p \n\n", &variable);
+		 printf(" Static Local Var. num1 : 0x%p \n", &num1);
+		 printf(" Static Local Var. num2 : 0x%p \n\n", &num2);
+		 printf(" Read-Only Data Var. str : 0x%p \n\n", str);
+		 printf(" Local Var. add  : 0x%p \n", &add);
+		 printf(" Local Var. sub  : 0x%p \n", &sub);
+		 printf(" Local Var. num3 : 0x%p \n", &num3);
+		 printf(" Local Var. num4 : 0x%p \n\n", &num4);
+		 printf(" Dinamic Var. arr : 0x%p \n", &arr);
+		 printf(" Dinamic Var. arr[0] : 0x%p \n", &arr[0]);
+		 printf(" Dinamic Var. arr[1] : 0x%p \n", &arr[1]);
+		 printf(" Dinamic Var. arr[2] : 0x%p \n", &arr[2]);
+
+*/
+
+#endif
